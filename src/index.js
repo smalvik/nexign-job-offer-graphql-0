69e593c59550ab18ccf50269d955c64da38b92ee
Vitaly Smirnov <vitaly.smirnov@nexign.com>
2023-08-03 19:37:40 +0100

    Добавляем в проект экземпляр Apollo Server

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
+import { resolvers } from "./resolvers";
+
+export const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`GraphQL server is running: ${url}`);
+};
+
+startServer();
